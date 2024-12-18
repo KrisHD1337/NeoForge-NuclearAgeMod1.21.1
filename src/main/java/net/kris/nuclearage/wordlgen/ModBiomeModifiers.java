8c9e6a352934a51596a665cd8c34b9efe8b691d1
IM23a-cernik <cernik@bzz.ch>
2024-12-18 13:31:33 +0100

    Added Worldgen setup

---
+++ src/main/java/net/kris/nuclearage/wordlgen/ModBiomeModifiers.java
@@ -0,0 +1,23 @@
+package net.kris.nuclearage.wordlgen;
+
+import net.kris.nuclearage.NuclearAge;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.neoforged.neoforge.common.world.BiomeModifier;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+
+public class ModBiomeModifiers {
+
+    public static void bootstrap(BootstrapContext<BiomeModifier> context) {
+        var placedFeatures = context.lookup(Registries.PLACED_FEATURE);
+        var biomes = context.lookup(Registries.BIOME);
+
+    }
+
+    public static ResourceKey<BiomeModifier> registerKey(String name) {
+        return ResourceKey.create(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ResourceLocation.fromNamespaceAndPath(NuclearAge.MODID, name));
+    }
+}
