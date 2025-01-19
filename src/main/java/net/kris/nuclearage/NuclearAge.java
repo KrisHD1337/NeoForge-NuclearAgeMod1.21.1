be36ace36b1e66c49f6b06ec315b78ef9be02ed3
IM23a-cernik <cernik@bzz.ch>
2025-01-19 12:21:59 +0100

    Updated the mod version

--- src/main/java/net/kris/nuclearage/NuclearAge.java
+++ src/main/java/net/kris/nuclearage/NuclearAge.java
@@ -4,25 +4,11 @@ import net.kris.nuclearage.block.ModBlocks;
 import net.kris.nuclearage.effect.ModEffects;
 import net.kris.nuclearage.item.ModCreativeModeTabs;
 import net.kris.nuclearage.item.ModItems;
-import net.kris.nuclearage.util.ModEventHandler;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -35,10 +21,6 @@ import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(NuclearAge.MODID)
@@ -58,8 +40,6 @@ public class NuclearAge
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
-
-        NeoForge.EVENT_BUS.register(new ModEventHandler());
 
         ModEffects.register(modEventBus);
 
